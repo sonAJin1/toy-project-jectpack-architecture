87a3d1ab9cb79b1600ffd21ed76bf9cb2b1e5dd0
sonAjin <sonajin11@gmail.com>
2021-06-14 23:03:12 +0900

    init toy project

---
+++ app/src/main/java/kr/co/ahjin/toy_jectpack_architecture/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.ahjin.toy_jectpack_architecture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
